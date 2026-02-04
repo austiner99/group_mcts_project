47d0717cef2a144e2d7afea7a398a30f1eaf2918
Austin <austiner@byu.edu>
2026-02-04 12:03:04 -0700

    baselinee random and greedy policy created (more can be created and these can be modified

--- baselines.py
+++ baselines.py
@@ -1,1 +1,36 @@
 # Code for random and greedy policies for comparison with mcts algorithm.
+import random
+
+def random_policy(env, state):
+    """
+    A policy that selects actions uniformly at random.
+    """
+    return random.choice(env.action_space)
+
+def greedy_policy(env, state):
+    """
+    A simple greedy policy that moves the agent closer to the goal.
+    Assumes the environment has 'agent_pos' and 'goal_pos' attributes.
+    """
+    agent_pos, goal_pos, _ = state
+    best_action = None
+    min_distance = float('inf')
+    
+    for action in env.action_space:
+        x, y = agent_pos
+        if action == 'up':
+            new_pos = (x, y + 1)
+        elif action == 'down':
+            new_pos = (x, y - 1)
+        elif action == 'left':
+            new_pos = (x - 1, y)
+        elif action == 'right':
+            new_pos = (x + 1, y)
+        else:
+            continue
+        
+        dist = abs(new_pos[0] - goal_pos[0]) + abs(new_pos[1] - goal_pos[1])
+        if dist < min_distance:
+            min_distance = dist
+            best_action = action
+    return best_action
